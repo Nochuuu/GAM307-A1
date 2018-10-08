6dc74aae9def8d7e764ca063a5a5c88a86c9360f
John Buizon <donjuanbuizon@gmail.com>
2018-10-08 16:55:39 +1100

    GameManager & UI

---
+++ Assessment 1 Base -  2018.2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private float _timeRemaining;
+
+    public float TimeRemaining
+    {
+        get { return _timeRemaining; }
+        set { _timeRemaining = value; }
+    }
+
+    private float maxTime = 2 * 60; // In seconds.
+
+    // Use this for initialization
+    void Start()
+    {
+        TimeRemaining = maxTime;
+    }
+
+    void Update()
+    {
+        TimeRemaining -= Time.deltaTime;
+
+        if (TimeRemaining <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            TimeRemaining = maxTime;
+        }
+    }
+}
---
+++ Assessment 1 Base -  2018.2/Assets/Scripts/UpdateUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateUI : MonoBehaviour {
+
+    [SerializeField]
+    private GUIText timerLabel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timerLabel.text = FormatTime(GameManager.Instance.TimeRemaining);
+	}
+
+    private string FormatTime(float timeInSeconds)
+    {
+        return string.Format("{0} : {1:00}", Mathf.FloorToInt(timeInSeconds / 60), Mathf.FloorToInt(timeInSeconds % 60));
+    }
+}
